9898d5d04a2396a59199239e034270e8b5551761
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2021-11-26 01:07:47 +0700

    i dont use gdm anymore

--- gpu-nightmare.sh
+++ gpu-nightmare.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-yay -S gdm-prime
+# yay -S gdm-prime
 
-sudo rm -rf etc/gdm/custom.conf
-sudo cp configs/gdm/custom.conf /etc/gdm/custom.conf
+# sudo rm -rf etc/gdm/custom.conf
+# sudo cp configs/gdm/custom.conf /etc/gdm/custom.conf
 
 yay -S optimus-manager optimus-manager-qt
 sudo cp configs/optimus-manager/optimus-manager.conf /etc/optimus-manager/optimus-manager.conf
