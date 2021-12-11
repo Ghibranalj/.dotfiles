b6d71790f0ceb1c2df67cef58694a433181f3594
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2021-12-11 17:33:00 +0100

    mouse fix

--- .on-new-kbd.sh
+++ .on-new-kbd.sh
@@ -2,8 +2,18 @@
 # from https://unix.stackexchange.com/questions/523635/prevent-keyboard-layout-reset-when-usb-keyboard-is-plugged-in#answer-523959
 
 
-echo >&2 "$@"
+#echo >&2 "$@"
 event=$1 id=$2 type=$3
+
+
+#echo $4 > ~/inputplug.log
+
+if [[ "$4" == *"Basilisk X HyperSpeed"* ]]
+then
+#	echo "its caught" >> ~/inputplug.log
+	exit
+fi
+
 
 case "$event $type" in
 'XIDeviceEnabled XISlaveKeyboard')
