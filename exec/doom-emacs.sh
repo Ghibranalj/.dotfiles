02d710fbb1ce3a0f8037ade0d7a21c547c575cad
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2022-07-08 19:21:51 +0200

    emacs(fix): change emacs to native & eaf install

--- exec/doom-emacs.sh
+++ exec/doom-emacs.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 # installing emacs
-yay -S emacs
+yay -S emacs-nativecomp
 
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
 git clone --depth=1 -b master https://github.com/emacs-eaf/emacs-application-framework.git ~/.emacs.d/site-lisp/emacs-application-framework/
+
+cd ~/.emacs.d/site-lisp/emacs-application-framework/
+chmod +x install-eaf.py
+./install-eaf.py
+cd -
 
 ~/.emacs.d/bin/doom install
