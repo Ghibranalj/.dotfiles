e065751173dd78c0e3f67b460660f46016d3026e
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2022-07-17 16:58:22 +0200

    [emacs] fix multiple cursor keybinding

--- doom/.doom.d/keymap.el
+++ doom/.doom.d/keymap.el
@@ -40,8 +40,22 @@
  ;;  :desc "Evaluate buffer" "b" #'eval-buffer
  ;;  :desc "Evaluate region" "r" #'eval-region
  ;;  :desc "Evaluate line" "l" #'+my/eval-line)
-
  (:prefix ("TAB" . "workspace")
   :desc "Save current worksppace to file" "S" #'+my/save-current-workspace
   )
+      ;;; <leader> m --- multiple cursors
+ (:when (featurep! :editor multiple-cursors)
+  (:prefix-map ("m" . "multiple-cursors")
+   :desc "Edit lines"         "l"         #'mc/edit-lines
+   :desc "Mark next"          "n"         #'mc/mark-next-like-this
+   :desc "Unmark next"        "N"         #'mc/unmark-next-like-this
+   :desc "Mark previous"      "p"         #'mc/mark-previous-like-this
+   :desc "Unmark previous"    "P"         #'mc/unmark-previous-like-this
+   :desc "Mark all"           "t"         #'mc/mark-all-like-this
+   :desc "Mark all DWIM"      "m"         #'mc/mark-all-like-this-dwim
+   :desc "Edit line endings"  "e"         #'mc/edit-ends-of-lines
+   :desc "Edit line starts"   "a"         #'mc/edit-beginnings-of-lines
+   :desc "Mark tag"           "s"         #'mc/mark-sgml-tag-pair
+   :desc "Mark in defun"      "d"         #'mc/mark-all-like-this-in-defun
+   :desc "Add cursor w/mouse" "<mouse-1>" #'mc/add-cursor-on-click))
  )
