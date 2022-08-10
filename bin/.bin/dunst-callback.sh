3e64b53be5ad2c4982eedd6ee3771d70b42b7b82
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2022-08-10 23:10:14 +0200

    [rofi] move notification processing to dunst-side

--- bin/.bin/dunst-callback.sh
+++ bin/.bin/dunst-callback.sh
@@ -1,14 +1,62 @@
 #!/usr/bin/env bash
 
+# this script has many dependencies, and is not meant to be run standalone
+# 1. fd
+# 2. gdbmtool
+# 3. gdbm
+#
+IGNORE="notify-send"
+
 DIR="$HOME/.cache/dunst"
 file="$DIR/dunst.log"
-
 mkdir -p "$DIR"
 touch "$file"
 
-IGNORE="pasystray notify-send"
+DATASTORE="$DIR/notificons.gdbm"
+
+put() {
+    KEY="$1"
+    VAL="$2"
+    gdbmcmd="store $KEY $VAL"
+    gdbmtool -s $DATASTORE <<<"$gdbmcmd"
+}
+get() {
+    KEY="$1"
+    gdbmcmd="fetch $KEY"
+    out=$(gdbmtool -s $DATASTORE <<<"$gdbmcmd")
+    [[ $out == *"No uch item found"* ]] && echo -n "" || echo "$out"
+}
+
+getIcon() {
+    ICON_NAME=$(echo "$1" | awk '{print tolower($0) }')
+    ICON_NAME=$(awk '{$1=$1}1' OFS="-" <<<"$ICON_NAME")
+    ICON=$(get "$ICON_NAME")
+    if [[ $ICON == "" ]]; then
+        ICON=$(fd -j8 -c never "$ICON_NAME" /usr/share/icons | grep apps | head -1)
+        if [ "$ICON" == "" ]; then
+            ICON=$(fd -j8 -1 -c never "$ICON_NAME" /usr/share/pixmaps)
+        fi
+        [[ $ICON == "" ]] && ICON="-"
+        put "$ICON_NAME" "$ICON"
+    fi
+
+    echo "$ICON"
+}
+
 if [[ $IGNORE == *"$1"* ]]; then
     exit 0
 fi
+ICON=$(getIcon "$1")
+if [[ "$ICON" == "-" ]]; then
+    TITLE="<big>$1</big> "
+fi
+#NOTiF="<i>$1</i>: $2 | <small>$3</small> <sub>$(date '+%H:%M %d %b')</sub> \x00icon\x1f$ICON"
+NOTIF="$TITLE<span>$2</span> : <small>$3</small> <sub>$(date '+%H:%M %d %b')</sub>\x00icon\x1f$ICON"
 
-echo "$1;;$2;;$3;;$(date '+%H:%M %d %b')" >> "$file"
+NOTIF=${NOTIF//\&/&amp;}
+
+if [[ "$(cat "$file")" == "" ]]; then
+    echo -e "$NOTIF" >"$file"
+else
+    sed -i "1 i$NOTIF" "$file"
+fi
