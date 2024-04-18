23869c3651090f868fbb445e3baf4500b2340040
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2024-04-18 15:40:54 +0700

    feat: add whatsapp-for-linux

--- home/.bin/screen.sh
+++ home/.bin/screen.sh
@@ -18,4 +18,5 @@ if [[ $HOSTNAME == "CreeprDell" ]]; then
   xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
   xrandr --addmode eDP-1 1920x1080_60.00
   xrandr -s 1920x1080_60.00
+  exit 0
 fi
--- home/.onStartup.sh
+++ home/.onStartup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 function start() {
+	if pgrep -f $1; then
+		echo "$1 is already running"
+		return
+	fi
+	# if the program doesnt exist, just return
+	if ! command -v $1 &> /dev/null; then
+		echo "$1 does not exist"
+		return
+	fi
 	$@ &
 	disown
 }
 
 start spotify
 start syndaemon -i 0.5 -t -k -R
+start whatsapp-for-linux
 sleep 2
 killall emacs
 systemctl restart --user blugon
